fb53461c0b0595e40bc4152807a7b8a2a3abe37b
c14180134 <c14180134@john.petra.ac.id>
2020-04-29 08:52:16 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
-  hasil=" ";
-
+  hasil:number;
+  hasilx="";
+  hasil1:number;
   dis(val){
-    this.hasil=this.hasil+val;    
+    this.hasil=this.hasil+val;
+    this.hasilx=this.hasilx+val;   
   }
+  simbol="";
+  pis(val){
+    this.simbol=val
+    this.hasil1=this.hasil;
+    this.hasil=0;
+  }
+  jawaban;
   solve(){
-    
+    if(this.simbol=='-'){
+      this.jawaban=this.hasil-this.hasil1;
+    }
+    if(this.simbol=='+'){
+      this.jawaban=this.hasil+this.hasil1;
+    }
+    if(this.simbol=='*'){
+      this.jawaban=this.hasil*this.hasil1;
+    }
   }
 }
 
